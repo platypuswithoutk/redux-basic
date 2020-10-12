6c130610fa7ca39af155c61ea401dfa9d496d659
Roksana Dzioba <roksanadzioba@gmail.com>
2020-10-12 22:00:47 +0200

    create store

--- src/App.js
+++ src/App.js
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { createStore } from 'redux';
+
+const initialMovies = {
+  movies: [
+    'Harry Potter',
+    'Władca pierścieni',
+    'A więc wojna',
+    'Poznaj moich Spartan'
+  ]
 }
+
+function movies(state = initialMovies, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+const store = createStore(movies)
+window.store = store;
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+}
+  
 
 export default App;
